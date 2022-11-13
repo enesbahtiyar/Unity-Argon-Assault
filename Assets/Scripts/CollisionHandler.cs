5f11dde5fe61bd65f0c02c4db08fce22be640f46
enesbahtiyar <enesbahtiyarserce@gmail.com>
2022-11-13 19:21:36 +0300

    optimized Particles and lasers

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = GetComponent<PlayerControls>();
+    }
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log(this.name + " collided with " + other.gameObject.name);
+        StartCrashSequence();
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{name} **Triggered By** {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    private void StartCrashSequence()
+    {
+        playerControls.enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
