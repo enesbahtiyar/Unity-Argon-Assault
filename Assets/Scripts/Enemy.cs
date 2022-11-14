2800909a329b4e13f82e7e70132db63f1b247527
enesbahtiyar <enesbahtiyarserce@gmail.com>
2022-11-14 15:25:27 +0300

    14.11.2022 first commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject deathVfx;
+    [SerializeField] private Transform parent;
     private void OnParticleCollision(GameObject other)
     {
+        GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(this.gameObject);
     }
 
