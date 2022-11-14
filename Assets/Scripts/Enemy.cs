dae8dc6cbd9c4699c31c21d736f4ff2551c7b3ed
enesbahtiyar <enesbahtiyarserce@gmail.com>
2022-11-14 18:45:04 +0300

    Enemy's created

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,28 +5,51 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject deathVfx;
-    [SerializeField] private Transform parent;
+    [SerializeField] private GameObject hitVfx;
+    GameObject ParentGameObject;
     Scoreboard scoreboard;
+    Rigidbody rb;
+
+    [SerializeField] int hitpoints = 20;
 
     private void Awake()
     {
+        
+        AddRigidBody();
         scoreboard = GameObject.Find("ScoreBoard").GetComponent<Scoreboard>();
+        ParentGameObject = GameObject.FindWithTag("VfxParent");
     }
+
+    private void AddRigidBody()
+    {
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if(hitpoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = ParentGameObject.transform;
         Destroy(this.gameObject);
     }
 
     private void ProcessHit()
     {
+  
+        GameObject vfx = Instantiate(hitVfx, transform.position, Quaternion.identity);
+        vfx.transform.parent = ParentGameObject.transform;
+        
+        hitpoints--;
         scoreboard.increaseScore(10);
+      
     }
 }
